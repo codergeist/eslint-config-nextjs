015637d13be2dbc4eb65e32d63ebc97d949dc7d4
Iker Garitaonandia <igaritaonandia@maark.com>
2024-04-20 01:03:38 +0200

    Simplify dependencies according to next.js docs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,3 @@
 module.exports = {
-  extends: [
-    'plugin:@next/next/recommended',
-    'plugin:react/recommended',
-    'airbnb',
-    'airbnb/hooks',
-    'plugin:prettier/recommended',
-  ],
-  settings: {
-    'import/resolver': {
-      alias: {
-        extensions: ['.js', '.jsx'],
-        map: [['@', './src']],
-      },
-    },
-  },
-  rules: {
-    // Not enforced
-    'import/prefer-default-export': 0,
-    'react/prop-types': 0,
-    'react/jsx-filename-extension': 0,
-  },
+  extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
 }
